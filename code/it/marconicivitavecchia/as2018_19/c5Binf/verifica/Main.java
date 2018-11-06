1d8da514bf71c8d08aa45ee9c89423b870a0eb60
Your Name <you@example.com>
2018-11-06 10:34:48 +0100

    CONSEGNA COMPITO

--- code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Binf/verifica/Main.java
@@ -1,6 +1,6 @@
 
 // TODO Inserire il package corretto
-package __TODO__;
+package mono;
 
 import javax.swing.*;
 
@@ -11,16 +11,16 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(500,300);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		super.StringBuilder()
 		// TODO Creare la stringa HTML per il wireframe
 		
 		// TODO Convertire lo StringBuilder in String
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(Almanacco astronomico));
 		super.setVisible(true);
 		 
 		System.out.println(htmlText);
